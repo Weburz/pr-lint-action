18d83f3cab5cc78faa3f575bf77b652d2540f137
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2023-10-06 12:32:14 +0530

    chore: recompile the binary files

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,20 @@
-/******/ /* webpack/runtime/compat */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	/* webpack/runtime/compat */
 /******/
-/******/ if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = new URL('.', import.meta.url).pathname.slice(import.meta.url.match(/^file:\/\/\/\w:/) ? 1 : 0, -1) + "/";
+/******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";
 /******/
 /************************************************************************/
 var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it uses a non-standard name for the exports (exports).
+(() => {
+var exports = __webpack_exports__;
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
 console.log('Hello World!');
+
+})();
+
+module.exports = __webpack_exports__;
+/******/ })()
+;
