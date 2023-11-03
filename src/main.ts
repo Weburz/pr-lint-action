6ac22fa3fa38e16145d4d54aa053d9f63cd225e7
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2023-11-03 14:27:14 +0530

    chore: add more experimental code for future reference

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,43 @@
-import * as github from "@actions/github";
+/**
+NOTE: This is a WIP project and is not ready yet!
+
+Things to-do:
+  1. Ensure if a "commitling.config.js|ts" file exists then use it instead of
+     custom configurations
+  2. Ensure the project is bug free
+  3. Write some tests for the project
+*/
+import lintPullRequestTitle from "./linter";
+import { error, getInput, setFailed } from "@actions/core";
+import { context, getOctokit } from "@actions/github";
 
 async function run() {
-  // Check if the workflow was triggered by a Pull Request event
-  if (github.context.eventName === "pull_request") {
-    const pullRequestTitle = github.context.payload.pull_request?.title;
+  try {
+    const token = getInput("GITHUB_TOKEN");
+    const octokit = getOctokit(token);
+    const pullRequestContext = context.payload.pull_request;
 
-    console.log(`Pull Request Title: ${pullRequestTitle}`);
-  } else {
-    console.log("This workflow was not triggered by a Pull Request event.");
+    if (!pullRequestContext) {
+      error(
+        `This action can only be invoked in "pull_request" or
+        "pull_request_target" events!`
+      );
+    }
+
+    const owner = pullRequestContext?.base.user.login;
+    const repo = pullRequestContext?.base.repo.name;
+
+    const { data: pullRequest } = await octokit.rest.pulls.get({
+      owner: owner,
+      repo: repo,
+      pull_number: pullRequestContext?.number || 0
+    });
+
+    await lintPullRequestTitle(pullRequest.title);
+  } catch (error) {
+    if (error instanceof Error) {
+      setFailed(error.message);
+    }
   }
 }
 
